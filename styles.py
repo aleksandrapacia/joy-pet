f47779cd1d8327da0953d51256152348fc2638ce
Aleksadra Pacia <olusiaOP05@gmail.com>
2022-01-31 14:38:53 +0100

    positione title label

--- styles.py
+++ styles.py
@@ -16,7 +16,8 @@ ScreenManager:
         on_press: root.manager.current = 'updatescreen'
     MDLabel:
         text: 'JoyPet'
-        pos_hint: {"x":0.8, "top":0.8}
+        pos_hint: {'center_x': 0.97, 'center_y': 0.9}
+        text_style: 'h1'
 
         
 
